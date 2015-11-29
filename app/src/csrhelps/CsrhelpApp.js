652175ff16780cf709ee34ebefaa57e466a5fa62
wangkang317 <jalexander.hc.317@gmail.com>
2015-11-29 04:47:07 +0100

    csrhelp feedback

--- app/src/csrhelps/CsrhelpApp.js
+++ app/src/csrhelps/CsrhelpApp.js
@@ -1,13 +1,12 @@
 	'use strict';
   angular
-      .module('csrhelpApp', ['ngMaterial', 'csrhelps', 'ngMessages'])
-      .config(function($mdThemingProvider, $mdIconProvider){						
-              
+      .module('csrhelpApp', ['ngMaterial', 'csrhelps', 'ngMessages','zeroclipboard'])
+      .config(function($mdThemingProvider, $mdIconProvider, uiZeroclipConfigProvider){						              
               $mdThemingProvider.theme('docs-dark', 'default')
                   .primaryPalette('yellow')
                   .dark();
-
-							
+                  
+              uiZeroclipConfigProvider.setZcConf({swfPath: 'assets/js/ZeroClipboard.swf'});
 							  // configure html5 to get links working on jsfiddle
 							  //$locationProvider.html5Mode(true);
-      })
+      });
--- app/src/csrhelps/CsrhelpService.js
+++ app/src/csrhelps/CsrhelpService.js
@@ -13,34 +13,8 @@
    * @constructor
    */
   function CsrhelpService($q){
-    var menu_items = [
-      {
-        name: 'About Us',
-        avatar: 'svg-1',
-        link: 'Aboutus',
-        content: 'SSL Certificate signing request tool-Requesting a certificate made simple'
-      },
-      {
-        name: 'Certificate Request',
-        avatar: 'svg-2',
-        link: 'CertificateRequest',
-        content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
-      },
-      {
-        name: 'Faqs',
-        avatar: 'svg-3',
-        link: 'Faqs',
-        content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
-      }
-    ];
-
     // Promise-based API
-    return {
-      loadAllMenuItems : function() {
-        // Simulate async nature of real remote calls
-        return $q.when(menu_items);
-      }
-    };
+    
   }
 
 })();
