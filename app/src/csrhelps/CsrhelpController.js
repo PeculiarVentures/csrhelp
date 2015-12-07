c13d42f18a6c7aa38af9c7b640aa1a77a654044f
Ryan Hurst <ryan_hurst@hotmail.com>
2015-12-07 11:30:18 -0800

    Update CsrhelpController.js

--- app/src/csrhelps/CsrhelpController.js
+++ app/src/csrhelps/CsrhelpController.js
@@ -672,7 +672,6 @@
 	    		else
 	    			$scope.certificate.filehostname = $scope.certificate.hostname;
 
-	                //console.log('data:application/pkcs8;base64,'+encodeURIComponent($scope.messages[6].privateKey));
 			$($event.currentTarget).attr('href','data:application/pkcs8,'+encodeURIComponent($scope.messages[6].privateKey)).attr('download', $scope.certificate.filehostname+'.key');
 
 		}
